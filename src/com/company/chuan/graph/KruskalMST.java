ea93eb6d70af53cc1c007803a3128143b1787e55
hyc <huangyichuan@wshifu.com>
2020-03-23 14:29:22 +0800

    生成最小生成树 kruskal 算法

--- src/com/company/chuan/graph/KruskalMST.java
+++ src/com/company/chuan/graph/KruskalMST.java
@@ -1,5 +1,9 @@
 package com.company.chuan.graph;
 
+import com.company.chuan.IndexMinPQ;
+import com.company.chuan.UF.QuickUnion;
+import com.company.chuan.UF.UF;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,6 +11,25 @@ public class KruskalMST {
     private Queue<Edge> mst;
     public KruskalMST(EdgeWeightedGraph G) {
         mst = new LinkedList<>();
-        MinPQ
+        IndexMinPQ<Edge> pq = new IndexMinPQ<>(G.V());
+
+        UF uf = new QuickUnion(G.V());
+
+        while(!pq.isEmpty() && mst.size() < G.V() - 1) {
+            Edge min = pq.min();
+            pq.delMin();
+
+            int v = min.either(), w = min.other(v);
+
+            if(uf.connected(v, w)) continue;
+            uf.union(v, w);
+            mst.add(min);
+        }
+
     }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
 }
