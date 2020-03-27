4a0c4d1123fab7067974eca583e191c3d35db212
dive2Pro <toylucklebo@gmail.com>
2020-03-27 17:59:52 +0800

    单词查找树

---
+++ src/com/company/chuan/string/StringST.java
@@ -0,0 +1,33 @@
+package com.company.chuan.string;
+
+public abstract class  StringST<Value> {
+    void put(String key, Value val){};
+    abstract Value get(String key);
+    void delete(String key){}
+    abstract boolean contains(String key);
+    abstract boolean isEmpty();
+
+    /**
+     *
+     * @param s
+     * @return s 的前缀中最长的键
+     */
+    abstract String longestPrefixOf(String s);
+
+    /**
+     *
+     * @param s
+     * @return 所有以 s 为前缀的键
+     */
+    abstract Iterable<String> keysWithPrefix(String s);
+
+    /**
+     *
+     * @param s
+     * @return 所有和 s 匹配的键 ( "." 能够匹配任意字符)
+     */
+    abstract Iterable<String> keysThatMatch(String s);
+
+    abstract int size();
+    abstract Iterable<String> keys();
+}
---
+++ src/com/company/chuan/string/TrieST.java
@@ -0,0 +1,180 @@
+package com.company.chuan.string;
+
+import java.util.Stack;
+
+public class TrieST<Value> extends StringST<Value> {
+    private static int R = 256;
+    private static int OMIT = ".".charAt(0);
+    private Node root;
+
+    static class Node {
+        Node[] next;
+        Object val;
+
+        public Node() {
+            next = new Node[R];
+
+        }
+    }
+    public TrieST() {
+
+    }
+
+    public void put(String key, Value val) {
+        root = put(root, key, val, 0);
+    }
+
+    private Node put(Node x, String key, Value val, int d) {
+
+        if(x == null) x = new Node();
+
+        if(d == key.length()) {
+            x.val = val;
+            return x;
+        }
+
+        int c = key.charAt(d);
+        x.next[c] = put(x.next[c], key, val, d + 1);
+
+        return x;
+    }
+
+    public Value get(String key) {
+        Node x = get(root, key, 0);
+        if (x == null) return null;
+
+        return (Value) x.val;
+    }
+
+
+    private Node get(Node x, String key, int d) {
+        if(x == null) return x;
+
+        if(key.length() == d) {
+            return x;
+        }
+
+        char c = key.charAt(d);
+
+        x = get(x.next[c], key, d + 1);
+
+
+        return x;
+    }
+
+    @Override
+    boolean contains(String key) {
+        return false;
+    }
+
+    @Override
+    boolean isEmpty() {
+        return root == null;
+    }
+
+    @Override
+    String longestPrefixOf(String s) {
+        return keysThatMatch(s, s.length()).pop();
+    }
+
+    @Override
+    Iterable<String> keysWithPrefix(String s) {
+        return keysThatMatch(s);
+    }
+
+    @Override
+    Iterable<String> keysThatMatch(String s) {
+        return keysThatMatch(s, s.length());
+    }
+
+    private Stack<String> keysThatMatch(String s, int size) {
+        return collect(root, s, 0);
+    }
+
+    @Override
+    int size() {
+        if(isEmpty()) {
+            return 0;
+        }
+
+        return collect(root, "", 0).size();
+    }
+
+
+    @Override
+    Iterable<String> keys() {
+        return keysWithPrefix("");
+    }
+
+    private Stack<String> collect(Node x, String key, int d) {
+        return collect(x, key, d, new Stack<>());
+    }
+
+    private Stack<String> collect(Node x, String key, int d, Stack<String> stack) {
+        for(int i = 0; i < x.next.length; i ++) {
+            Node n = x.next[i];
+            if(n == null) {
+                continue;
+            }
+
+
+            if(!key.equals("")) {
+                if(key.length() <= d) {
+
+                } else {
+                    int v = key.charAt(d);
+                    if( v != OMIT && v != i) {
+                        continue;
+                    }
+                }
+            }
+
+            if(n.val != null) {
+                stack.push((String) n.val);
+            }
+
+
+            collect(n, key, d + 1, stack);
+        }
+        return stack;
+    }
+
+    private int collect(Node x) {
+        int count = 0;
+        for(int i = 0; i < x.next.length; i ++) {
+           Node n = x.next[i];
+           if(n == null) {
+               continue;
+           }
+
+           if(n.val != null) {
+               count ++;
+           }
+           count += collect(n);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+       TrieST<String> trieST = new TrieST<>();
+       String[] strings = new String[]{
+               "She",
+               "Shell",
+               "Sea",
+               "Dota",
+               "Doddle",
+               "Axe"
+       };
+
+       for(String s: strings) {
+           trieST.put(s, s);
+       }
+
+//       System.out.println(trieST.size());
+//        System.out.println(trieST.keysWithPrefix("..e"));
+//        System.out.println(trieST.keysWithPrefix("Dod"));
+        System.out.println(trieST.longestPrefixOf("Shel"));
+
+    }
+}
